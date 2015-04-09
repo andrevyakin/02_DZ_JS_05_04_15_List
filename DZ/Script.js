fc2b2f6a6b8282e960cac8a958a8efeefe6d2542
andrevyakin <andrevyakin@gmail.com>
2015-04-09 18:31:11 +0300

    Сделал список и кнопку
    Подключил Bootstrap

--- DZ/Script.js
+++ DZ/Script.js
@@ -8,3 +8,34 @@
  *  То есть на каждом пункту меню будет разный цвет.
  *  Все остальные пункты, на которых не происходит замена в данный момент, должны быто одного цвета.*/
 
+var button = document.getElementById ("button");
+button.style.background = "yellow";
+button.style.marginLeft = "14%";
+button.style.marginTop = "2%";
+
+var myList = document.getElementById("List");
+myList.style.margin = "5%";
+myList.style.paddingLeft = "7%";
+myList.style.width = "300px";
+myList.style.fontSize = "250%";
+myList.style.background = "green";
+
+myStr = new Array (10);
+
+for (var i = 1; i < 11; i++) {
+    myStr[i - 1] = document.getElementById('li' + i);
+    myStr[i - 1].style.paddingLeft = "20px";
+}
+
+var timer;
+
+
+
+function Clock ()
+{
+   timer = window.setInterval('Show()', 2000) ;
+}
+
+
+
+document.write("Проверка")
